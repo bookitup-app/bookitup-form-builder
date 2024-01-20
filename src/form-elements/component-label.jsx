af2b964e5cf19a2fb15b1b1903ca0a1b595f254a
jburdel <janburdel1995@gmail.com>
2024-01-20 10:57:49 +0100

    required-field: show * (star) instead of badge

--- src/form-elements/component-label.jsx
+++ src/form-elements/component-label.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import myxss from './myxss';
-import IntlMessages from '../language-provider/IntlMessages';
 
 const ComponentLabel = (props) => {
   const hasRequiredLabel = (props.data.hasOwnProperty('required') && props.data.required === true && !props.read_only);
@@ -11,7 +10,7 @@ const ComponentLabel = (props) => {
   return (
     <label className={props.className || 'form-label'}>
       <span dangerouslySetInnerHTML={{ __html: labelText }}/>
-      {hasRequiredLabel && <span className="label-required badge badge-danger"><IntlMessages id='required' /></span>}
+      {hasRequiredLabel && <span style={{ color: 'red', fontSize: 18 }}>{'  '}*</span>}
     </label>
   );
 };
